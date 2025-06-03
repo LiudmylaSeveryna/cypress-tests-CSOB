cacb78f3b98509ba73b49eea8d4309ec6410d389
Liudmyla Severyna <lyusitta@meta.ua>
2025-06-03 12:56:01 +0200

    AddedRegistration

---
+++ cypress/e2e/registration.cy.ts
@@ -0,0 +1,68 @@
+import executRegistation, {} from "..//pages/lide/registrationForm"  
+import lidePage from "..//pages/lide/lidePage"
+
+  describe('validation the filling of the registration form', () => {
+    beforeEach(() => { // ignores error "uncaught:exception"
+        cy.on('uncaught:exception', (err, runnable) => {
+        return false;
+        }); 
+        cy.visit('/konto-hned');
+        cy.wait(3000); 
+        lidePage.cookieConsent();
+        //executRegistation.confirmSubmission().click()
+        })
+   
+    it('registation on form - invalid name', () => {
+        cy.fixture('userRegistrationForm.json').then((userName) => {
+        executRegistation.nameInput().type(userName.nameInvalid)
+        executRegistation.surnameInput().type(userName.surnameValid)
+        executRegistation.phoneInput().type(userName.phoneNumberValid)
+        executRegistation.emailInput().clear().type(userName.emailValid)
+        executRegistation.nameError().should('be.visible').and('contain.text', 'Zadejte minimálně 2 znaky');
+        })
+    })
+
+    it('registation on form - invalid surname', () => {
+        cy.fixture('userRegistrationForm.json').then((userName) => {
+        executRegistation.nameInput().type(userName.nameValid)
+        executRegistation.surnameInput().type(userName.surnameInvalid)
+        executRegistation.phoneInput().type(userName.phoneNumberValid)
+        executRegistation.emailInput().clear().type(userName.emailValid)
+        executRegistation.surnameError().should('be.visible').and('contain.text', 'Zadejte minimálně 2 znaky');
+        })
+    })
+        
+    it('registation on form - invalid phone', () => {
+        cy.fixture('userRegistrationForm.json').then((userName) => {
+        executRegistation.nameInput().type(userName.nameValid)
+        executRegistation.surnameInput().type(userName.surnameValid)
+        executRegistation.phoneInput().type(userName.phoneNumberInvalid)
+        executRegistation.emailInput().clear().type(userName.emailValid)
+        executRegistation.phoneError().should('be.visible').and('contain.text', 'Zadejte platné telefonní číslo');
+        })
+    })
+
+    it('registation on form - invalid email', () => {
+        cy.fixture('userRegistrationForm.json').then((userName) => {
+        executRegistation.nameInput().type(userName.nameValid)
+        executRegistation.surnameInput().type(userName.surnameValid)
+        executRegistation.phoneInput().type(userName.phoneNumberValid)
+        executRegistation.emailInput().clear().type(userName.emailInvalid)
+        cy.get('body').click();
+        executRegistation.emailError().should('be.visible').and('contain.text', 'Zadejte platný e-mail');
+        })
+    })
+
+    it('registation on form - valid all fields', () => {
+        cy.fixture('userRegistrationForm.json').then((userName) => {
+        executRegistation.nameInput().type(userName.nameValid)
+        executRegistation.surnameInput().type(userName.surnameValid)
+        executRegistation.phoneInput().type(userName.phoneNumberValid)
+        executRegistation.emailInput().clear().type(userName.emailValid)
+        executRegistation.buttonPokracovat().click()
+        executRegistation.bannerSuccess().should('be.visible').and('contain.text', 'Sjednání Plus Konta');
+        })
+    })
+
+
+})
---
+++ cypress/pages/lide/registrationForm.ts
@@ -0,0 +1,19 @@
+export default new class RegistrFormPage{
+    confirmSubmission = () => cy.get('button[class="html-button js-exponea-close"]')
+
+    nameInput = () => cy.get('input[id="onb-lead-page-name-input"]')
+    surnameInput = () => cy.get('input[id="onb-lead-page-surname-input"]')
+    phoneInput = () => cy.get('input[id="onb-lead-page-phone-input"]')
+    emailInput = () => cy.get('input[id="onb-lead-page-email-input"]')
+
+    nameError = () => cy.get('#onb-lead-page-name-input').closest('.crct-c-frm1001').find('.crct-information-tooltip')
+    surnameError = () =>  cy.get('#onb-lead-page-surname-input').closest('.crct-c-frm1001').find('.crct-information-tooltip')
+    phoneError = () => cy.get('#onb-lead-page-phone-input').closest('.crct-c-frm1001').find('.crct-information-tooltip')
+    emailError = () => cy.get('#onb-lead-page-email-input').closest('.crct-c-frm1001').find('.crct-information-tooltip')
+
+    buttonPokracovat = () => cy.get('button[class="crct-frm1006-button"]')
+    bannerSuccess = () => cy.get('div[class="rct-b-banner-content pui-picturefill-background pui-js-picturefill-background-init-rct"]')
+
+    
+    
+}
